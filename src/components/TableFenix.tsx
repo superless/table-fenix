52ba83b5ca244c67243ec28e07ede31a4eff862c
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-04-03 00:15:09 -0300

    PascalCase

--- src/components/TableFenix.tsx
+++ src/components/TableFenix.tsx
@@ -11,7 +11,7 @@ export interface ITableFenixProps {
 }
 
 function GetEntityHeaders(entities: IEntitySearch[], header: (header: number) => string) {
-  let headersRelated = entities.reduce((pn: Number[], u) => [...pn, ...u.rel.filter(a => a.name !== "").map(s => s.entityIndex)], []);
+  let headersRelated = entities.reduce((pn: Number[], u) => [...pn, ...u.rel.filter(a => a.Name !== "").map(s => s.EntityIndex)], []);
   return headersRelated.filter((n, i) => headersRelated.indexOf(n) === i).map(s => ({ index: s, name: header(s as number) }));
 }
 
@@ -19,28 +19,28 @@ function GetPropHeaders(entities: IEntitySearch[], typeRelated:Related, header: 
   let headersProperties : Number[] = [];
   switch(typeRelated){
     case Related.SUGGESTION:
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.sug.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.sug.map(s => s.PropertyIndex)], []);
       break;
     case Related.STR:      
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.str.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.str.map(s => s.PropertyIndex)], []);
       break;
     case Related.BOOL :
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.bl.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.bl.map(s => s.PropertyIndex)], []);
       break;
     case Related.NUM32:
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.num32.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.num32.map(s => s.PropertyIndex)], []);
     case Related.NUM64:
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.num64.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.num64.map(s => s.PropertyIndex)], []);
       break;
     case Related.DATE:
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.dt.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.dt.map(s => s.PropertyIndex)], []);
       break;
     case Related.DBL:
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.dbl.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.dbl.map(s => s.PropertyIndex)], []);
     case Related.ENUM : 
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.enum.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.enum.map(s => s.PropertyIndex)], []);
     case Related.GEO :
-      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.geo.map(s => s.propertyIndex)], []);
+      headersProperties = entities.reduce((pn: Number[], u) => [...pn, ...u.geo.map(s => s.PropertyIndex)], []);
   }
 
   if (headersProperties.length>0)
@@ -50,7 +50,7 @@ function GetPropHeaders(entities: IEntitySearch[], typeRelated:Related, header: 
 }
 
 export default function TableFenix(props: ITableFenixProps) {
-  let entities = !props.elements ? [] : props.elements.entities;
+  let entities = !props.elements ? [] : props.elements.Entities;
   
 
   const suggestHeaders = GetPropHeaders(entities, Related.SUGGESTION, props.headerProperty);
@@ -99,38 +99,38 @@ export default function TableFenix(props: ITableFenixProps) {
       <Table.Body>
 
         {entities!.map(entity =>
-          <Table.Row key={entity.id}>
+          <Table.Row key={entity.Id}>
             {suggestHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.SUGGESTION as number}.${h.index}_${entity.id}`}> {entity.sug.filter(e => e.propertyIndex === h.index).length > 0 ? entity.sug.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.SUGGESTION as number}.${h.index}_${entity.Id}`}> {entity.sug.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.sug.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {strHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.STR as number}.${h.index}_${entity.id}`}> {entity.str.filter(e => e.propertyIndex === h.index).length > 0 ? entity.str.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.STR as number}.${h.index}_${entity.Id}`}> {entity.str.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.str.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {num32Headers.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.NUM32 as number}.${h.index}_${entity.id}`}> {entity.num32.filter(e => e.propertyIndex === h.index).length > 0 ? entity.num32.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.NUM32 as number}.${h.index}_${entity.Id}`}> {entity.num32.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.num32.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {num64Headers.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.NUM64 as number}.${h.index}_${entity.id}`}> {entity.num64.filter(e => e.propertyIndex === h.index).length > 0 ? entity.num64.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.NUM64 as number}.${h.index}_${entity.Id}`}> {entity.num64.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.num64.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {doubleHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.DBL as number}.${h.index}_${entity.id}`}> {entity.dbl.filter(e => e.propertyIndex === h.index).length > 0 ? entity.dbl.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.DBL as number}.${h.index}_${entity.Id}`}> {entity.dbl.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.dbl.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {boolHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.BOOL as number}.${h.index}_${entity.id}`}> {entity.bl.filter(e => e.propertyIndex === h.index).length > 0 ? entity.bl.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.BOOL as number}.${h.index}_${entity.Id}`}> {entity.bl.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.bl.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {enumHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.ENUM as number}.${h.index}_${entity.id}`}> {entity.enum.filter(e => e.propertyIndex === h.index).length > 0 ? entity.enum.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.ENUM as number}.${h.index}_${entity.Id}`}> {entity.enum.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.enum.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {dtHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.DATE as number}.${h.index}_${entity.id}`}> {entity.dt.filter(e => e.propertyIndex === h.index).length > 0 ? entity.dt.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.DATE as number}.${h.index}_${entity.Id}`}> {entity.dt.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.dt.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
             {geoHeaders.map(h => (
-              <Table.Cell textAlign='center' key={`${Related.GEO as number}.${h.index}_${entity.id}`}> {entity.geo.filter(e => e.propertyIndex === h.index).length > 0 ? entity.geo.filter(e => e.propertyIndex === h.index)[0].value : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`${Related.GEO as number}.${h.index}_${entity.Id}`}> {entity.geo.filter(e => e.PropertyIndex === h.index).length > 0 ? entity.geo.filter(e => e.PropertyIndex === h.index)[0].Value : ""}</Table.Cell>
             ))}
 
 
             {entityHeader.map(h => (
-              <Table.Cell textAlign='center' key={`e${h.index}_${entity.id}`} > {entity.rel.filter(e => e.entityIndex === h.index).length > 0 ? entity.rel.filter(e => e.entityIndex === h.index)[0].name : ""}</Table.Cell>
+              <Table.Cell textAlign='center' key={`e${h.index}_${entity.Id}`} > {entity.rel.filter(e => e.EntityIndex === h.index).length > 0 ? entity.rel.filter(e => e.EntityIndex === h.index)[0].Name : ""}</Table.Cell>
             ))}
           </Table.Row>
         )}

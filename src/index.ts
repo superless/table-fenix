8f695985742921de6f80e2627025a0431020b0af
Cristian Rojas <cristian.rojas.uv@trifenix.io>
2020-05-06 23:09:58 -0400

    Probando

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ import {SearchFenix, ISearchFilterProps} from './components/SearchFenix';
 import {ISearchBaseModel} from './components/search/base/model/types/ISearchBaseModel';
 import { ISearchCategoryModel } from './components/search/base/model/types/ISearchCategoryModel';
 
-
 export { TableFenix, SearchFenix, ISearchFilterProps, ISearchBaseModel, ISearchCategoryModel };

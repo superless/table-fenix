8e32423139e6e7640fcab50d16ae46bdba92a06f
trifenix <alejandro.iglesas@trifenix.io>
2020-05-18 12:26:02 -0400

    borrado componente boton test.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import TableFenix from './components/TableFenix';
 import {SearchFenix, ISearchFilterProps} from './components/SearchFenix';
 import {ISearchBaseModel} from './components/search/base/model/types/ISearchBaseModel';
 import { ISearchCategoryModel } from './components/search/base/model/types/ISearchCategoryModel';
-import ButtonTest from './components/buttonTest';
 
-export { ButtonTest, TableFenix, SearchFenix, ISearchFilterProps, ISearchBaseModel, ISearchCategoryModel };
+
+export { TableFenix, SearchFenix, ISearchFilterProps, ISearchBaseModel, ISearchCategoryModel };
